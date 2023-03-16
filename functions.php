0edf7b4705b8546ea41db662f32bc30025d08766
Christoffer Wallman <christoffer.wallman@studerande.karlskogafhs.se>
2023-03-16 15:54:04 +0100

    Add menu

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Register Navigation Locations
+ * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+ * 
+ * Registers our locations for our header and sidebar menu. 
+ * This will add a link to menus under appearence
+ */ 
+
+ if ( ! function_exists( 'bdocs_register_nav_menu' ) ) {
+
+	function bdocs_register_nav_menu(){
+		register_nav_menus( array(
+	    	'header_menu' => __( 'Header Menu', 'bdocs_domain' ),
+	    	'sidebar_menu'  => __( 'Sidebar Menu', 'text_domain' ),
+		) );
+	}
+	add_action( 'after_setup_theme', 'bdocs_register_nav_menu', 0 );
+}
--- index.php
+++ index.php
@@ -4,19 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project 5 - Doc Theme</title>
+    <title><?php bloginfo('name') ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="style.css">
-    <script src="src/js/darkMode.js" defer></script>
-    <script src="src/js/toc.js" defer></script>
-    <script src="src/js/sidebar.js" defer></script>
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style.css">
+    <script src="<?= get_template_directory_uri() ?>/src/js/darkMode.js" defer></script>
+    <script src="<?= get_template_directory_uri() ?>/src/js/toc.js" defer></script>
+    <script src="<?= get_template_directory_uri() ?>/src/js/sidebar.js" defer></script>
+    <?php wp_head() ?>
 </head>
-
-
-<body>
+<body <?php body_class() ?>>
 
 <div class="container">
 
@@ -24,18 +23,34 @@
     <header class="doc-header">
         <div class="doc-header__inner">
 
-            <a href="" class="branding">Bdocs</a>
+            <a href="<?= home_url ( )  ?>" class="branding"><?php bloginfo('name') ?></a>
 
             <div class="doc-header__content">
 
                 <!-- Header Navigation -->
-                <nav class="header-nav">
+                <!-- <nav class="header-nav">
                     <ul class="header-nav__list">
                         <li class="header-nav__item header-nav__item--current"><a href="" class="header-nav__link">Documentation</a></li>
                         <li class="header-nav__item"><a href="" class="header-nav__link">Showcase</a></li>
                         <li class="header-nav__item"><a href="" class="header-nav__link">About</a></li>
                     </ul>
-                </nav>
+                </nav> -->
+
+                <?php 
+                /**
+                 * Get header menu from wordpress
+                 * @link https://developer.wordpress.org/reference/functions/wp_nav_menu/
+                 */
+
+                $args = array(
+                    'theme_location' => 'header_menu', // Location for menu registered in our functions.php
+                    'container' => 'nav', // Change div to nav
+                    'container_class' => 'header-nav', // Add class to container,
+                    'menu_class' => 'header-nav__list' // Change class for ul 
+                 );
+
+                 wp_nav_menu( $args );
+                ?>
 
                 <div class="search-form">
                     <form action="" class="search-form__form">
@@ -244,11 +259,10 @@
     <!-- Footer -->
     <footer class="doc-footer">
         <div class="doc-footer__inner">
-            <span class="doc-footer__copyright"> &copy; 2023 Bdocs, All rights reserved</span>
+            <span class="doc-footer__copyright"> &copy; 2023 <?php bloginfo('name') ?>, All rights reserved</span>
         </div>
     </footer>
 </div>
-
-    
+<?php wp_footer() ?>
 </body>
 </html>
