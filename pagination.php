e9e273032083f7b062111dbcfc6c0ca8e3c12d43
Christoffer Wallman <christoffer.wallman@studerande.karlskogafhs.se>
2023-03-20 21:21:24 +0100

    add pagination

--- pagination.php
+++ pagination.php
@@ -1,4 +1,7 @@
 <nav class="pagination">
-                        <a href="" class="pagination__link pagination__link--prev">Doc Theme</a>
-                        <a href="" class="pagination__link pagination__link--next">Page Configuration</a>
-                    </nav>
+    <?php previous_post_link('%link') ?>
+    <?php next_post_link('%link') ?>
+
+    <!-- <a href="" class="pagination__link pagination__link--prev">Doc Theme</a> -->
+    <!-- <a href="" class="pagination__link pagination__link--next">Page Configuration</a> -->
+</nav>
