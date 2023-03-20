fc17e8ed33c1d95f27f2e61a156d23baa02b36e1
Christoffer Wallman <christoffer.wallman@studerande.karlskogafhs.se>
2023-03-20 21:17:13 +0100

    Add updates loop

--- index.php
+++ index.php
@@ -14,18 +14,26 @@
 
                     <h1>Documentation</h1>
                 </header>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptatum error delectus voluptatibus fuga tenetur aut possimus facere earum voluptas vel sit magnam esse aperiam laudantium temporibus quam sed quis.
-                    Aliquid nam, in commodi sapiente fugit tenetur doloremque nemo nihil cumque recusandae mollitia ad asperiores perferendis! Error odit doloribus molestiae sint deserunt qui, nemo aspernatur blanditiis fugiat dicta sed porro.
-                    Adipisci commodi ipsa ducimus architecto aut voluptatem omnis cumque perferendis sunt autem perspiciatis ipsum fuga molestiae dolorum consectetur reiciendis quia, necessitatibus facilis eaque dolores laudantium voluptas minus reprehenderit? Tempore, earum?
-                    Deserunt, voluptatem! Culpa corporis laborum consequuntur dolorum tempore voluptatem, nam eius quas asperiores repellendus assumenda incidunt molestias similique cupiditate consequatur quisquam deleniti mollitia consectetur fuga soluta. Illo odio libero ad?
-                    Possimus incidunt accusamus culpa maiores delectus. Atque velit id, eaque totam aperiam molestiae vitae perspiciatis reiciendis sed quaerat at magni a ullam reprehenderit doloremque illo debitis minus voluptates cum! Assumenda?</p>
+
+                <h2>Latest updates:</h2>
+                <!-- Page content -->
+                <?php if ( have_posts() ) : while ( have_posts()) : the_post();?>
+
+                    <div class="post">
+                        <span class="post__date"><?php the_time('j F, Y - H:i'); ?></span>
+                        <h3 class="post__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3></a>
+                        <div class="post__content">
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; endif; ?>
 
                 <footer class="page__footer">
                     <div class="page__lastupdated">Last updated on February 20, 2023</div>
                 </footer>
             </article>
 
-            <?php get_template_part('toc') ?>
+            <?php //get_template_part('toc') ?>
 
         </div>
     </main>
